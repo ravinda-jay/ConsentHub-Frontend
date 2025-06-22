3532261819d1b715239c36762172c6868f272c58
Ojitha Rajapaksha <ojitha.20230522@iit.ac.lk>
2025-06-22 17:34:37 +0530

    Base URI updated

--- src/services/agreementService.ts
+++ src/services/agreementService.ts
@@ -1,6 +1,6 @@
 import { Agreement } from '../types/Agreement';
 
-const API_BASE_URL = 'https://agreement-management-api-production.up.railway.app/agreements';
+const API_BASE_URL = 'https://agreement-management-backend-production.up.railway.app/agreements';
 
 export const agreementService = {
   async getAllAgreements(params?: {
